57ea4c8b0eb9ed50dc0cadd12d62d3b3bd0b925f
kbberker <kbbuk90@gmail.com>
2019-01-03 11:24:49 +0000

    Added create and destroy methods to player controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -3,4 +3,37 @@ class Api::V1::PlayersController < ApplicationController
     @players = Player.all
     render json: @players
   end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    if @player
+      render json: @player
+    else
+      render json: {error: 'Player not found.'}, status: 400
+    end
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      render json: @player
+    else
+      render json: {error: 'Unable to create player.'}, status: 400
+    end
+  end
+
+  def destroy
+    @player = Player.find_by(id: params[:id])
+    if @player
+      @player.destroy()
+      render json: {message: 'User sucessfully deleted.'}
+    else
+      render json: {error: 'Player not found.'}, status: 400
+    end
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
